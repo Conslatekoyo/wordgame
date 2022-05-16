45a5ba744ace416812596b081dc4bb10e521fc33
Conslatekoyo <conslatekoyo@gmail.com>
2022-05-16 14:37:11 +0300

    first commit

---
+++ madlib.py
@@ -0,0 +1,7 @@
+adj= input("Adjective: ")
+verb1= input("verb: ")
+verb2=("verb: ")
+famous_person= input("Famous person ")
+
+madlib =f"computer programming is so gg{adj}!It males me so {verb1} all the time because I love to {verb2}. stay hydrated like you are {famous_person}"
+print(madlib)
