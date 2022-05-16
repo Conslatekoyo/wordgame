ced9bc5201f8bc052c6a7e3f9f457b46e6fa3122
Conslatekoyo <conslatekoyo@gmail.com>
2022-05-16 15:05:12 +0300

    first commit

--- madlib.py
+++ madlib.py
@@ -3,5 +3,16 @@ verb1= input("verb: ")
 verb2=("verb: ")
 famous_person= input("Famous person ")
 
-madlib =f"computer programming is so gg{adj}!It males me so {verb1} all the time because I love to {verb2}. stay hydrated like you are {famous_person}"
+madlib=f"computer programming is so gg{adj}!It males me so {verb1} all the time because I love to {verb2}code like I am {famous_person}"
 print(madlib)
+
+
+adj2= input("Adjective: ")
+adj3= input("Adjective: ")
+verb3= input("verb: ")
+verb4=("verb: ")
+famous_person2= input("Famous person ")
+madlib2=f"Following the launch of the first space rocket to the moon, the sight was{adj2}. Everyone wanted a glimpse of this {adj3} event. the world{verb3} in wonder.Charles Johnson, an American photographer,{verb4}his way admist of the public to get a glimpse of the sight. The rocket wenyt up as though it was {famous_person2}"
+print(madlib2)
+print(madlib2)
+
